f614621ffc85c5f0e082d4bf6463db951dab24ef
Matteo Scanferla <scanferla935@gmail.com>
2025-12-09 19:15:35 +0100

    update startup script: change main.py path and update entrypoint to server/index.mjs

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# 1. Avvia il tuo script Python in background
 echo "[CUSTOM-INIT] Starting Firewall Manager (main.py)..."
 python3 /etc/wireguard/scripts/main.py &
 
-# 2. Passa il controllo all'entrypoint originale di wg-easy
-# Questo avvia WireGuard e la Web UI
 echo "[CUSTOM-INIT] Starting WG-Easy..."
-exec /usr/bin/dumb-init node server.js
+exec /usr/bin/dumb-init node server/index.mjs
