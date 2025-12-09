9c3884831b29da553a6d676ab9fd1888f8e9117d
Matteo Scanferla <scanferla935@gmail.com>
2025-12-09 19:34:06 +0100

    update startup script: comment out main.py execution and retain WG-Easy startup

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-echo "[CUSTOM-INIT] Starting Firewall Manager (main.py)..."
-python3 /etc/wireguard/scripts/main.py &
+#
+#echo "[CUSTOM-INIT] Starting Firewall Manager (main.py)..."
+#python3 /etc/wireguard/scripts/main.py &
 
 echo "[CUSTOM-INIT] Starting WG-Easy..."
 exec /usr/bin/dumb-init node server/index.mjs
