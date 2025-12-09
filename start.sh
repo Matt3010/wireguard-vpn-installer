d5f90c64286c5d7b76d4759ac8c392efaa1e7ff5
Matteo Scanferla <scanferla935@gmail.com>
2025-12-09 19:04:40 +0100

    update Docker configuration: move main.py to a new location and adjust startup script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # 1. Avvia il tuo script Python in background
 echo "[CUSTOM-INIT] Starting Firewall Manager (main.py)..."
-python3 /app/main.py &
+python3 /etc/wireguard/scripts/main.py &
 
 # 2. Passa il controllo all'entrypoint originale di wg-easy
 # Questo avvia WireGuard e la Web UI
