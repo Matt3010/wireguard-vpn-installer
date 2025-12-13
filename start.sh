bddcf7ce7fc216f100f6833e87484604ce42fb68
Matteo Scanferla <scanferla935@gmail.com>
2025-12-13 03:06:12 +0100

    update(start.sh): add directory listing and run main.py in the background

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+ls -la /app/
+
 echo "[CUSTOM-INIT] Starting Firewall Manager (main.py)..."
-python3 /app/main.py &
+nohup python3 -u /app/main.py > /proc/1/fd/1 2>/proc/1/fd/2 &
 
 echo "[CUSTOM-INIT] Starting WG-Easy..."
 exec /usr/bin/dumb-init node server/index.mjs
